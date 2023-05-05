4e6c50403125081cf0f14b5c61c1c5daba900d24
Holger Luide <holger.luide@tktk.ee>
2023-05-05 15:54:15 +0300

    add week3 code

---
+++ IfDayOfTheWeek/Program.cs
@@ -0,0 +1,33 @@
+﻿
+int Weekday = Convert.ToInt32(DateTime.Now.DayOfWeek);
+
+if(Weekday == 0) 
+{
+    Console.WriteLine("It's Sunday!");
+}
+else if (Weekday == 1)
+{
+    Console.WriteLine("It's Monday!");
+}
+else if (Weekday == 2)
+{
+    Console.WriteLine("It's Tuesday!");
+}
+else if (Weekday == 3) 
+{
+    Console.WriteLine("It's Wednesday!");
+}
+else if (Weekday == 4)
+{
+    Console.WriteLine("It's Thursday!");
+}
+else if (Weekday == 5)
+{
+    Console.WriteLine("It's Friday!");
+}
+else
+{
+    Console.WriteLine("It's Saturday!");
+}
+
+Console.WriteLine("Have a nice day!");
